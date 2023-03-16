0e9b2657730a3714fe76a15d4089e8c6f2e143c4
LeaClover <plesen_14@mail.ru>
2023-03-16 18:26:11 +0300

    add student file

---
+++ HW3/Students.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB.alg_and_data_str.HW3 {
+    public  class Student {
+        public string SurName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public int Group { get; set; }
+
+        public override string ToString() {
+            return $"{SurName} {FirstName} {Birthday.ToShortDateString()} - {Group}";
+        }
+    }
+}
