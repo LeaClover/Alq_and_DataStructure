77a64043addbd6dc120b3655869f28302facd1a9
LeaClover <plesen_14@mail.ru>
2023-03-20 20:08:09 +0300

    add project file

---
+++ HW4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+namespace GB.alg_and_data_str.HW4 {
+
+    class Program {
+        static void Main(string[] args) {
+
+            RedBlackTree rbt = new RedBlackTree();
+            Console.WriteLine("Введите количество узлов которые хотите добавить в дерево: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значения узлов через Enter: ");
+            for (int i = 0; i < num; i++) {
+                int value = Convert.ToInt32(Console.ReadLine());
+                rbt.add(value);
+            }
+            Console.WriteLine("finish");
+            rbt.DisplayTree(1);
+        }
+
+        
+    }
+}
